e04fa049b613098264a7825de7a90f570c91e5fb
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-08-18 14:01:24 +0900

    feat: Create Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import * as S from './style'
+
+function Footer() {
+  return (
+    <S.Footer>
+      <p>내 지역보기</p>
+      <p>전체 시도보기</p>
+      <p>즐겨찾기</p>
+    </S.Footer>
+    // <div>
+    //   <p>내 지역보기</p>
+    //   <p>전체 시도보기</p>
+    //   <p>즐겨찾기</p>
+    // </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Footer = styled.footer`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  position: absolute;
+  bottom: 0;
+`
