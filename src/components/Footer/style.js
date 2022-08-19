a9a334d58795b65c1435e2748fddf858434892a5
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-08-20 03:27:29 +0900

    feat: Add Footer Style

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -2,10 +2,23 @@ import styled from 'styled-components'
 
 export const Footer = styled.footer`
   width: 100%;
-  height: 60px;
+  height: 80px;
   display: flex;
   align-items: center;
   justify-content: space-around;
-  position: absolute;
+  position: fixed;
   bottom: 0;
+  background-color: #fff;
+  font-size: 18px;
+  .footer-nav {
+    text-decoration: none;
+    color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .nav-title {
+      margin-top: 6px;
+    }
+  }
 `
