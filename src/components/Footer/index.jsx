df5cfc4f78be9da7eb01d8f305b4a6ac082ea986
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-08-19 20:35:49 +0900

    feat: Add Link path

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import * as S from './style'
+import { AiFillStar } from 'react-icons/ai'
+import { BiMap } from 'react-icons/bi'
+import { GrMapLocation } from 'react-icons/gr'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
     <S.Footer>
-      <p>내 지역보기</p>
-      <p>전체 시도보기</p>
-      <p>즐겨찾기</p>
+      <Link className="footer-nav" to="/">
+        <BiMap />
+        <p className="nav-title">내 지역보기</p>
+      </Link>
+      <Link className="footer-nav" to="/all">
+        <GrMapLocation />
+        <p className="nav-title">전체 시도보기</p>
+      </Link>
+      <Link className="footer-nav" to="/favorite">
+        <AiFillStar />
+        <p className="nav-title">즐겨찾기</p>
+      </Link>
     </S.Footer>
-    // <div>
-    //   <p>내 지역보기</p>
-    //   <p>전체 시도보기</p>
-    //   <p>즐겨찾기</p>
-    // </div>
   )
 }
 
