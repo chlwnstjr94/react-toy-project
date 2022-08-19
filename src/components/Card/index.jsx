d29ede9a02f16097f668ef66d2ae295be86f6eec
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-08-19 20:36:26 +0900

    feat: Add Card and Card style

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import * as S from './style'
+
+const pm10Grade = {
+  1: '좋음',
+  2: '보통',
+  3: '나쁨',
+  4: '매우 나쁨',
+  5: '치명적',
+}
+
+const pm10GradeColor = {
+  1: '#70A1D7',
+  2: '#6BCB77',
+  3: '#FFD93D',
+  4: '#FF6B6B',
+  5: '#C21010',
+}
+
+function Card(data) {
+  return (
+    <S.Card color={data.pm10Grade ? pm10GradeColor[data.pm10Grade] : '#9e9e9e'}>
+      <div className="card-title">
+        <h3 className="station-name">{data.stationName}</h3>
+        <span className="sido-name">{data.sidoName}</span>
+      </div>
+      <p className="dust-grade">
+        {data.pm10Grade ? pm10Grade[data.pm10Grade] : '데이터 없음'}
+      </p>
+      <p className="dust-value">미세먼지 수치 : {data.pm10Value}</p>
+      <p className="dust-time">{data.dataTime}</p>
+    </S.Card>
+  )
+}
+
+export default Card
---
+++ src/components/Card/style.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const Card = styled.section`
+  width: 80%;
+  background-color: ${(props) => props.color};
+  margin: 20px auto;
+  padding: 20px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  color: #fff;
+  p {
+    margin: 10px 0;
+  }
+  .card-title {
+    display: flex;
+    align-items: end;
+    margin: 10px 0;
+    .station-name {
+      font-size: 20px;
+      margin-right: 10px;
+    }
+    .sido-name {
+      font-size: 14px;
+    }
+  }
+  .dust-grade {
+    background-color: #fff;
+    font-size: 30px;
+    padding: 10px;
+    border-radius: 10px;
+    color: ${(props) => props.color};
+  }
+`
