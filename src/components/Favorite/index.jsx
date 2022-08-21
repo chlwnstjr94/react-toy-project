bf3f60c56004d922ceb78df5d55b7f254bb8d108
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-08-22 04:08:03 +0900

    feat: Add Favorite page

--- src/components/Favorite/index.jsx
+++ src/components/Favorite/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux/es/exports'
+import { favoriteArr } from '../../store/dust'
+import Card from '../Card'
+import * as S from './style'
 
 function Favorite() {
+  const datas = useSelector(favoriteArr)
+
   return (
-    <div>
-      <h1>즐겨찾기</h1>
-    </div>
+    <S.Favorite>
+      <h1 className="title">즐겨찾기</h1>
+
+      {datas && datas.map((item, index) => <Card key={index} info={item} />)}
+    </S.Favorite>
   )
 }
 
---
+++ src/components/Favorite/style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Favorite = styled.main`
+  width: 100%;
+  height: 100%;
+  padding-bottom: 80px;
+`
