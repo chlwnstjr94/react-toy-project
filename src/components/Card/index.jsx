2b853a9d60d972573192dce99e8dc419b04b90e9
chlwnstjr94 <chlwnstjr940404@gmail.com>
2022-08-22 04:07:32 +0900

    feat: Modify api connet way & Add favorite

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { AiOutlineStar } from 'react-icons/ai'
+import { AiOutlineStar, AiFillStar } from 'react-icons/ai'
+import { useDispatch, useSelector } from 'react-redux'
+import { addMyFavoriteList, getSidoDatas } from '../../store/dust'
 import * as S from './style'
 
 const pm10Grade = {
@@ -18,26 +20,42 @@ const pm10GradeColor = {
   5: '#C21010',
 }
 
-function isClicked() {}
+function Card({ info }) {
+  const dispatch = useDispatch()
+  const datas = useSelector(getSidoDatas)
+  const data = datas[info]
 
-function Card(data) {
+  const addFavorite = () => {
+    dispatch(addMyFavoriteList(data.stationName))
+  }
+
   return (
-    <S.Card color={data.pm10Grade ? pm10GradeColor[data.pm10Grade] : '#9e9e9e'}>
-      <div className="card-header">
-        <div className="card-title">
-          <h3 className="station-name">{data.stationName}</h3>
-          <span className="sido-name">{data.sidoName}</span>
-        </div>
-        <div className="header-icon">
-          <AiOutlineStar />
-        </div>
-      </div>
-      <p className="dust-grade">
-        {data.pm10Grade ? pm10Grade[data.pm10Grade] : '데이터 없음'}
-      </p>
-      <p className="dust-value">미세먼지 수치 : {data.pm10Value}</p>
-      <p className="dust-time">{data.dataTime}</p>
-    </S.Card>
+    <>
+      {data && (
+        <S.Card
+          color={data.pm10Grade ? pm10GradeColor[data.pm10Grade] : '#9e9e9e'}
+        >
+          <div className="card-header">
+            <div className="card-title">
+              <h3 className="station-name">{data.stationName}</h3>
+              <span className="sido-name">{data.sidoName}</span>
+            </div>
+            <div className="header-icon" onClick={addFavorite}>
+              {data.myFavorite ? <AiFillStar /> : <AiOutlineStar />}
+            </div>
+          </div>
+          <p className="dust-grade">
+            {data.pm10Grade ? pm10Grade[data.pm10Grade] : '데이터 없음'}
+          </p>
+          <p className="dust-value">
+            미세먼지 수치 : {data.pm10Value ? data.pm10Value : '-'}
+          </p>
+          <p className="dust-time">
+            {data.dataTime ? data.dataTime : '데이터 없음'}
+          </p>
+        </S.Card>
+      )}
+    </>
   )
 }
 
